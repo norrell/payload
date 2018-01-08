4a149e28dc1a657a7c45500c134d1ad9ead7d53b
Enrico <enrico.zattarin@gmail.com>
2018-01-08 18:41:04 +0100

    Remove old files

--- beacon.h
+++
@@ -1,9 +0,0 @@
-#ifndef HTTP_CLIENT_BEACON_H
-#define HTTP_CLIENT_BEACON_H
-
-//extern int ID;
-#include "command.h"
-
-char *get_beacon(void);
-int get_commands(char *beacon_response, struct command **cmds);
-#endif
